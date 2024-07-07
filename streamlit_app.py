9a35970e1cbc6bf84daf59ee56c1bac2b140fd84
mohamed-thanveer-offl <136612144+mohamed-thanveer-offl@users.noreply.github.com>
2024-07-07 11:42:01 +0000

    read csv feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
-import streamlit as st
+# import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# st.title("🎈 My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+import pandas as pd
+import streamlit as st
+uploaded_file = st.file_uploader('Choose a file')
+if uploaded_file is not None:
+#read csv
+    df1=pd.read_csv(uploaded_file)
